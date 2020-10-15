35d38baab20da78ccafe7b2a541f9403cbb05e34
shinaz_m <42809705+shinaz-m@users.noreply.github.com>
2020-10-15 17:46:18 +0530

    Update Constants.java

--- src/main/java/com/example/firebaseDemo/common/Constants.java
+++ src/main/java/com/example/firebaseDemo/common/Constants.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Constants {
-    public static final String FIREBASE_SDK_JSON ="firebase-adminsdk.json";
-    public static final String FIREBASE_BUCKET = "sample-e05e3.appspot.com";
-    public static final String FIREBASE_PROJECT_ID ="sample-e05e3";
+    public static final String FIREBASE_SDK_JSON ="firebase-adminsdk.json";//copy the sdk-config file root address, if its in root ,filename is enough
+    public static final String FIREBASE_BUCKET = "";//enter your bucket name
+    public static final String FIREBASE_PROJECT_ID ="";//enter your project id
 }
